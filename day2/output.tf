34bafb59f4a90020c0ba046f1c3f93852052e382
nikhil <nikhiljadhav@78953>
2024-08-24 00:19:02 +0530

    modified

---
+++ day2/output.tf
@@ -0,0 +1,8 @@
+output "publicip" {
+  value = aws_instance.name.public_ip
+sensitive = false
+}
+output "privateip" {
+    value = aws_instance.name.private_ip
+    sensitive = true
+}
---
+++ day2/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "value"
+  type = string
+  default = ""
+}
+variable "instance_type" {
+  description = "value"
+  type = string
+  default = "t2.nano"
+}
+variable "key_name" {
+  description = "my keyname"
+  type = string
+  default = ""
+}
